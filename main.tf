95476c59d4a4bc3993833acd1fe5d1609c22cfec
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-26 15:35:48 +0530

    changes

--- main.tf
+++ main.tf
@@ -7,13 +7,15 @@ resource "aws_flow_log" "example" {
 
 resource "aws_vpc" "office_vpc" {
     cidr_block = "50.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
     tags = {
       name = "office_vpc" 
     }
 }
 
 resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.issue_vpc.id
+  vpc_id = aws_vpc.office_vpc.id
   ingress {
     protocol  = "-1"
     self      = true
