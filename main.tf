16f70ca66ccd72636a12b5386d3bfce7c55e7138
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-26 16:04:52 +0530

    default sg

--- main.tf
+++ main.tf
@@ -16,16 +16,4 @@ resource "aws_vpc" "office_vpc" {
 
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.office_vpc.id
-  ingress {
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 0
-    to_port   = 0
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
