da96f9d5f388cadbdd7e42d0a071ff1ddd9d981f
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-26 15:13:07 +0530

    changes vpc as per checkov alerts

--- main.tf
+++ main.tf
@@ -1,7 +1,29 @@
-resource "aws_vpc" "office_vpc" {
+resource "aws_flow_log" "example" {
+  iam_role_arn    = "arn"
+  log_destination = "log"
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.office_vpc.id
+}
 
+resource "aws_vpc" "office_vpc" {
     cidr_block = "50.0.0.0/16"
     tags = {
-      name = "office_vpc"
+      name = "office_vpc" 
     }
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.issue_vpc.id
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
